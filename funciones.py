d36cddce54d11063e8c1a3c23d1dbf1dc8afd76f
g_lopez <g19.lopez2007@gmail.com>
2026-05-05 16:05:53 -0600

    Acomode de mejor manera las funciones para observarlas visualmente mejor en el orden que salen en el menu

--- funciones.py
+++ funciones.py
@@ -1,78 +1,12 @@
+#Creadores: Gustavo López Alvarado y Mel Acuña
+#Día de creación: 26/4/26
+#Última modificación: 5/5/26
+#Versión de Python: 3.14
+
 from funcionesAux import *
 import csv
 
-def mostrarTokens(pTokens):
-    '''
-    Funcionamiento:
-    -Entrada:
-        Se agarra la lita de tokens para mostrar los resultados ya guardados
-    -Salida:
-        Se muestra la lista de tokens
-    '''
-    resultado=mostrarTokensAux(pTokens)
-    if resultado!=True: #Mientras que el resultado sea diferente de True se imprime el mensaje
-        print(resultado)
-    else:
-        print("\nTokens cargados:")
-        i=0
-        while i<len(pTokens): 
-#Se agarra el primer elemento de la lista y luego se muesta el primer elemento de la tupla y se separa con el simbolo de "->" para poner el segundo elemento de la tupla
-            print(pTokens[i][0],"=",pTokens[i][1])
-            i+=1
-
-def agregarModificarTokens(pTokens,pPartes,pSeparador):
-    '''
-    Funcionamiento:
-    -Entrada:
-        Se agarra la lista de tokens, una cadena con los tokens que el usuario quiere
-        agregar o modificar y el separador usado para dividir la palabra original
-        del reemplazo.
-    -Salida:
-        Se muestra si el token fue agregado, actualizado o si ocurrió algún error.
-        Además, se retorna la lista de tokens actualizada.
-    '''
-    partes=pPartes.split(" ")  #Separa la cadena ingresada con espacio
-    i=0 #Contador para recorrer cada token separado
-    while i<len(partes):
-        token=partes[i].strip() #Guarda un token individual y elimina espacios al inicio y al final
-        validacion=agregarModificarTokensAux(token,pSeparador) #Guarda el resultado del Aux
-        if validacion!=True: 
-            print(validacion)
-            return pTokens #Retorna la lista sin cambios para evitar perder los tokens
-        elif pSeparador in token:
-            datos=token.split(pSeparador) #Separa el token en palabra original y reemplazo
-            if len(datos)==2: #Valida que existan exactamente dos partes
-                original=datos[0].strip()  #Guarda la palabra original sin espacios extra
-                nuevo=datos[1].strip() #Guarda el nuevo reemplazo sin espacios extra
-                posicion=buscarPosicionToken(original,pTokens) #Busca si la palabra original ya existe
-                if posicion!=-1: #Si la posición es diferente de -1, significa que el token ya existe
-                        _, _, contador = pTokens[posicion] #Lo que hace es una funcion que hace que no se tomen los otros 2 elementos de la tupla y solo se use el contador
-                        pTokens[posicion]=(original,nuevo,contador+1) #Actualiza el token existente con el nuevo reemplazo y aumenta el contador
-                        print("Token actualizado:",original)
-                else:
-                    pTokens.append((original,nuevo,1)) #Agrega el token nuevo como una tupla
-                    print("Token agregado:",original)
-        i+=1 #Avanza al siguiente token ingresado
-    return pTokens #Retorna la lista con los cambios realizados
-
-def guardarTokens(pTokens, nombre, separador):
-    '''
-    Funcionamiento:
-    -Entrada:
-        Se agarra la lista de tokens para guardarlos en un archivo de texto
-    -Salida:
-        Se muestra un mensaje indicando que los tokens fueron guardados exitosamente
-        o si ocurrió algún error.
-    '''
-    if guardarTokensAux(pTokens, nombre, separador)!=True:
-        print(guardarTokensAux(pTokens, nombre, separador))
-        return pTokens #Retorna la lista sin cambios para evitar perder los tokens
-    with open(nombre, "w") as archivo: #Abre el archivo en modo escritura
-            for token in pTokens: #Recorre cada token en la lista
-                linea=token[0] + separador + token[1] + "\n" #Crea una línea con el formato "original->nuevo"
-                archivo.write(linea) #Escribe la línea en el archivo
-    print("Tokens guardados")
-
+#Funcion de la opción 1:
 def cargarTokens(pTokens, nombre, separador):
     '''
     Funcionamiento:
@@ -109,21 +43,80 @@ def cargarTokens(pTokens, nombre, separador):
     archivo.close() #Cierra el archivo después de leerlo
     return pTokens #Retorna los tokens cargados
 
-def guardarCSV(pTokens):
+#Funcion de la opción 2:
+def mostrarTokens(pTokens):
     '''
-    Funcionamiwento:
+    Funcionamiento:
     -Entrada:
-        Se agarra la lista de tokens para guardarlos en un archivo CSV
+        Se agarra la lita de tokens para mostrar los resultados ya guardados
     -Salida:
-        En base a la lista de tokens se crea el CSV donde se agrega la ultima versión de cada token, y el contador de cuántas veces se ha actualizado cada token.
+        Se muestra la lista de tokens
     '''
-    with open("tokens.csv", mode="w", newline="", encoding="utf-8") as file:
-        writer = csv.writer(file)
-        writer.writerow(["La distribución es: Original, Token, Contador"])  #Escribe la cabecera del CSV
-        for original, nuevo, contador in pTokens: #Crea el como se tiene que ver
-            fila = f"{original}={nuevo}({contador})"
-            writer.writerow([fila])
+    resultado=mostrarTokensAux(pTokens)
+    if resultado!=True: #Mientras que el resultado sea diferente de True se imprime el mensaje
+        print(resultado)
+    else:
+        print("\nTokens cargados:")
+        i=0
+        while i<len(pTokens): 
+#Se agarra el primer elemento de la lista y luego se muesta el primer elemento de la tupla y se separa con el simbolo de "->" para poner el segundo elemento de la tupla
+            print(pTokens[i][0],"=",pTokens[i][1])
+            i+=1
 
+#Funcion de la opción 3:
+def agregarModificarTokens(pTokens,pPartes,pSeparador):
+    '''
+    Funcionamiento:
+    -Entrada:
+        Se agarra la lista de tokens, una cadena con los tokens que el usuario quiere
+        agregar o modificar y el separador usado para dividir la palabra original
+        del reemplazo.
+    -Salida:
+        Se muestra si el token fue agregado, actualizado o si ocurrió algún error.
+        Además, se retorna la lista de tokens actualizada.
+    '''
+    partes=pPartes.split(" ")  #Separa la cadena ingresada con espacio
+    i=0 #Contador para recorrer cada token separado
+    while i<len(partes):
+        token=partes[i].strip() #Guarda un token individual y elimina espacios al inicio y al final
+        validacion=agregarModificarTokensAux(token,pSeparador) #Guarda el resultado del Aux
+        if validacion!=True: 
+            print(validacion)
+            return pTokens #Retorna la lista sin cambios para evitar perder los tokens
+        elif pSeparador in token:
+            datos=token.split(pSeparador) #Separa el token en palabra original y reemplazo
+            if len(datos)==2: #Valida que existan exactamente dos partes
+                original=datos[0].strip()  #Guarda la palabra original sin espacios extra
+                nuevo=datos[1].strip() #Guarda el nuevo reemplazo sin espacios extra
+                posicion=buscarPosicionToken(original,pTokens) #Busca si la palabra original ya existe
+                if posicion!=-1: #Si la posición es diferente de -1, significa que el token ya existe
+                        _, _, contador = pTokens[posicion] #Lo que hace es una funcion que hace que no se tomen los otros 2 elementos de la tupla y solo se use el contador
+                        pTokens[posicion]=(original,nuevo,contador+1) #Actualiza el token existente con el nuevo reemplazo y aumenta el contador
+                        print("Token actualizado:",original)
+                else:
+                    pTokens.append((original,nuevo,1)) #Agrega el token nuevo como una tupla
+                    print("Token agregado:",original)
+        i+=1 #Avanza al siguiente token ingresado
+    return pTokens #Retorna la lista con los cambios realizados
+
+#Funcion de la opción 4:
+def guardarTokens(pTokens, nombre, separador):
+    '''
+    Funcionamiento:
+    -Entrada:
+        Se agarra la lista de tokens para guardarlos en un archivo de texto
+    -Salida:
+        Se muestra un mensaje indicando que los tokens fueron guardados exitosamente
+        o si ocurrió algún error.
+    '''
+    if guardarTokensAux(pTokens, nombre, separador)!=True:
+        print(guardarTokensAux(pTokens, nombre, separador))
+        return pTokens #Retorna la lista sin cambios para evitar perder los tokens
+    with open(nombre, "w") as archivo: #Abre el archivo en modo escritura
+            for token in pTokens: #Recorre cada token en la lista
+                linea=token[0] + separador + token[1] + "\n" #Crea una línea con el formato "original->nuevo"
+                archivo.write(linea) #Escribe la línea en el archivo
+    print("Tokens guardados")
 
 # Funcionalida 5: Traducir código
 def traducirArchivo(nombreEntrada, nombreSalida, pTokens):
@@ -151,7 +144,23 @@ def traducirArchivo(nombreEntrada, nombreSalida, pTokens):
     salida.close()
     print("Archivo traducido correctamente")
 
-# Funcionalidad 7: Generar reporte HTML
+#Funcion de la opción 6:
+def guardarCSV(pTokens):
+    '''
+    Funcionamiwento:
+    -Entrada:
+        Se agarra la lista de tokens para guardarlos en un archivo CSV
+    -Salida:
+        En base a la lista de tokens se crea el CSV donde se agrega la ultima versión de cada token, y el contador de cuántas veces se ha actualizado cada token.
+    '''
+    with open("tokens.csv", mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["La distribución es: Original, Token, Contador"])  #Escribe la cabecera del CSV
+        for original, nuevo, contador in pTokens: #Crea el como se tiene que ver
+            fila = f"{original}={nuevo}({contador})"
+            writer.writerow([fila])
+
+#Funcion de la opción 7:
 def generarHTML(pTokens):
     '''
     Funcionamiento:
@@ -182,4 +191,3 @@ def generarHTML(pTokens):
         print("Archivo HTML generado correctamente")
     except:
         print("Error al generar el HTML")
-
